bfb126fa1c5735bedfa0b0eb33bae48fbe5d1286
AlexMem <ss47305@gmail.com>
2019-02-24 22:49:36 +0300

    task3: task done

---
+++ tasks/task3/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApplication {
+    class Program {
+        private static void insertionSort(int[] array) {
+            for (int i = 1; i < array.Length; ++i) {
+                for (int j = i; j > 0; --j) {
+                    if (array[j] < array[j-1]) {
+                        array[j] += array[j-1];
+                        array[j-1] = array[j] - array[j-1];
+                        array[j] -= array[j-1];
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void outputArray(int[] array) {
+            for (int i = 0; i < array.Length; ++i) {
+                Console.Write(array[i] + " ");
+            }
+        }
+    
+        static void Main(string[] args) {
+            var array = new[]{-10, 1, 2, -1, 4, 8, 5, 6, 7, 0};
+            Console.WriteLine(array.Length);
+            insertionSort(array);
+            outputArray(array);
+        }
+    }
+}
